4fc1f8e2943ad4fbdd4f16c6e0a8e74a1590a91f
abcplusd <pavittar.singh@abcplusd.net>
2017-07-18 22:03:50 +0530

    first commit

--- src/SimpleGridExample.jsx
+++ src/SimpleGridExample.jsx
@@ -1,57 +1,171 @@
 import React, {Component} from "react";
 import {AgGridReact} from "ag-grid-react";
+// import MoodRenderer from "./MoodRenderer";
+// import MoodEditor from "./MoodEditor";
+import NumericEditor from "./NumericEditor";
 
 export default class extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            columnDefs: this.createColumnDefs(),
-            rowData: this.createRowData()
-        }
-    }
+    this.state = {
+      columnDefs: this.createColumnDefs(),
+      rowData: this.createRowData(),
+      emp1: '',
+      emp2: ''
+    };
 
-    onGridReady(params) {
-        this.gridApi = params.api;
-        this.columnApi = params.columnApi;
+    this.setEmp1 = this.setEmp1.bind(this);
+    this.setEmp2 = this.setEmp2.bind(this);
+  }
 
-        this.gridApi.sizeColumnsToFit();
-    }
+  onGridReady(params) {
+    this.gridApi = params.api;
+    this.columnApi = params.columnApi;
+    this.gridApi.sizeColumnsToFit();
+  }
 
-    createColumnDefs() {
-        return [
-            {headerName: "Make", field: "make"},
-            {headerName: "Model", field: "model"},
-            {headerName: "Price", field: "price"}
-        ];
-    }
+  createColumnDefs() {
+    return [
+      {
+        headerName: "EmployeeId",
+        field: "employeeId",
+        width: 300
+      }, {
+        headerName: "Name",
+        field: "name",
+        // cellRendererFramework: MoodRenderer,
+        // cellEditorFramework: MoodEditor,
+        editable: true,
+        width: 250
+      }, {
+        headerName: "ParentId",
+        field: "parentId",
+        cellEditorFramework: NumericEditor,
+        editable: true,
+        width: 250
+      }, {
+        headerName: "JoiningDate",
+        field: "joinDate",
+        cellEditorFramework: NumericEditor,
+        editable: true,
+        width: 250
+      }
+    ];
+  }
 
-    createRowData() {
-        return [
-            {make: "Toyota", model: "Celica", price: 35000},
-            {make: "Ford", model: "Mondeo", price: 32000},
-            {make: "Porsche", model: "Boxter", price: 72000}
-        ];
-    }
+  createRowData() {
+    return [
+      {
+        "name": "b",
+        "employeeId": 1005,
+        "parentId": 1000,
+        "joinDate": "2012-04-24"
+      }, {
+        "name": "e",
+        "employeeId": 1035,
+        "parentId": 1000,
+        "joinDate": "2014-07-12"
+      }, {
+        "name": "t",
+        "employeeId": 2001,
+        "parentId": 1000,
+        "joinDate": null
+      }
+    ];
+  }
 
-    render() {
-        let containerStyle = {
-            height: 115,
-            width: 500
-        };
+  componentDidMount() {
+    this.getUserList();
+  }
 
-        return (
-            <div style={containerStyle} className="ag-fresh">
-                <h1>Simple ag-Grid React Example</h1>
-                <AgGridReact
-                    // properties
-                    columnDefs={this.state.columnDefs}
-                    rowData={this.state.rowData}
+  getUserList() {
+    fetch('http://192.168.30.79:8000/phonetool/tree/1000', {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }
+    }).then((response) => response.json()).then((responseJson) => {
+      this.setState({rowData: responseJson.data});
+      console.log(responseJson.data);
+    }).catch((error) => {
+      console.error(error);
+    });
+  }
 
-                    // events
-                    onGridReady={this.onGridReady}>
-                </AgGridReact>
-            </div>
-        )
-    }
+  postUserList(data) {
+    fetch('http://192.168.30.79:8000/phonetool/createtree', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify(data)
+    }).then((response) => response.json()).then((responseJson) => {
+      responseJson.data && this.setState({rowData: responseJson.data});
+      console.log(responseJson.data);
+    }).catch((error) => {
+      console.error(error);
+    });
+  }
+
+  getShortestPath(emp1, emp2) {
+    fetch('http://192.168.30.79:8000/phonetool/shortestpath/' + emp1 + '/' + emp2, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }
+    }).then((response) => response.json()).then((responseJson) => {
+      responseJson.data && this.setState({rowData: responseJson.data});
+      console.log(responseJson.data);
+    }).catch((error) => {
+      console.error(error);
+    });
+  }
+
+  updateList() {
+    this.postUserList(this.state.rowData);
+    console.log(this.state.rowData);
+  }
+
+  shortestpath() {
+    console.log(this.state.emp1, this.state.emp2);
+    this.getShortestPath(this.state.emp1, this.state.emp2);
+  }
+
+  setEmp1(e) {
+    console.log(e.target.value);
+    this.setState({'emp1': e.target.value});
+  };
+  setEmp2(e) {
+    console.log(e.target.value);
+    this.setState({'emp2': e.target.value});
+  };
+
+  render() {
+    let containerStyle = {
+      height: 115,
+      width: 600
+    };
+
+    return (
+      <div style={containerStyle} className="ag-fresh">
+        <h1>Simple ag-Grid React Example</h1>
+        <AgGridReact // properties
+          columnDefs={this.state.columnDefs} rowData={this.state.rowData} // events
+          onGridReady={this.onGridReady}></AgGridReact>
+        <button onClick={() => this.updateList()}>update</button>
+        <form>
+          <input type="number" name="email" placeholder="employeeId" value={this.state.emp1} onChange={this.setEmp1}/>
+          <input type="number" name="password" placeholder="employeeId" value={this.state.emp2} onChange={this.setEmp2}/>
+          <button type="button" onClick={() => this.shortestpath()}>shortest path</button>
+        </form>
+      </div>
+    )
+  }
 };
